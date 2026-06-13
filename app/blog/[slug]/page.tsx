420894da9d8fd57294ddb789bbc816914b7d8c9d
jml123 <langjinming18@gmail.com>
2026-06-13 13:34:07 +0800

    SEO优化：文章详情页添加BreadcrumbList结构化数据
    为文章详情页添加BreadcrumbList schema，帮助搜索引擎理解页面层级结构

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -107,12 +107,38 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
     "articleSection": article.category,
     "wordCount": article.content.length,
   };
+  
+    const breadcrumbSchema = {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "name": "练学宝",
+          "item": "https://www.skillxm.cn/"
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "name": "知识分享",
+          "item": "https://www.skillxm.cn/blog/"
+        },
+        {
+          "@type": "ListItem",
+          "position": 3,
+          "name": article.title,
+          "item": "https://www.skillxm.cn/blog/${article.id}/"
+        }
+      ]
+    };
+  }
 
   return (
     <>
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(articleSchema) }}
+        dangerouslySetInnerHTML={{ __html: JSON.stringify([articleSchema, breadcrumbSchema]) }}
       />
       <BlogPostPage slug={slug} />
 
