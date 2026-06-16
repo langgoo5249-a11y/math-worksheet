a488f627334b3e0f4f8dfe987855b4bf406a7af6
jm6-lang <jm6-lang@users.noreply.github.com>
2026-06-16 07:33:58 +0000

    feat: 网站添加多语言与小程序提示
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -294,6 +294,20 @@ export default async function RootLayout({
   return (
     <html lang="zh-CN" className={notoSansSC.className}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WNQGWC4W');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <meta name="baidu-site-verification" content="codeva-nVZFsgvPZu" />
 
         {/* Google Search Console 站点验证 */}
@@ -410,6 +424,16 @@ export default async function RootLayout({
         />
       </head>
       <body className="min-h-screen antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WNQGWC4W"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NextIntlClientProvider messages={messages} locale="zh">
           <CookieConsent />
           {children}
